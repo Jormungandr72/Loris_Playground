21982fac4b7b908e5c1d74b0c46e45cf987bc620
Jormungandr72 <110646466+Jormungandr72@users.noreply.github.com>
2024-12-17 13:58:53 -0600

    Update MaterialCard.py
    Added a basic constructor while determining additional attributes the class should have.

--- Loris_Playground_Backend/gameplay_objects/cards/MaterialCard.py
+++ Loris_Playground_Backend/gameplay_objects/cards/MaterialCard.py
@@ -1,1 +1,6 @@
 # Contains information related to material deck cards.
+from Card import Card
+
+class MaterialCard(Card):
+    def __init__(self, name, types, classes, subtypes, element):
+        Card.__init__(self, name, types, classes, subtypes, element)
