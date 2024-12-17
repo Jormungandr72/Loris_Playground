856323b57794ee1e8832459f2e3d560384572b97
Jormungandr72 <110646466+Jormungandr72@users.noreply.github.com>
2024-12-17 14:00:16 -0600

    Update MaterialCard.py
    Rearranged and added comments.

--- Loris_Playground_Backend/gameplay_objects/cards/MaterialCard.py
+++ Loris_Playground_Backend/gameplay_objects/cards/MaterialCard.py
@@ -1,6 +1,7 @@
-# Contains information related to material deck cards.
 from Card import Card
 
+# Contains information related to material deck cards.
 class MaterialCard(Card):
+    # Constructors
     def __init__(self, name, types, classes, subtypes, element):
         Card.__init__(self, name, types, classes, subtypes, element)
